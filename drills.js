fa564ee3a344f764d2d79d7c39dd5c2971103ea9
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-12-18 13:48:50 -0600

    through 6

--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+const Queue = require('./Queue');
+
 /**
  * # 3
  * 
@@ -7,12 +9,12 @@
  * 
  */
 
- //0 - 999
+//0 - 999
 
- //input: 799, "Gone fishin";
- //output: index
+//input: 799, "Gone fishin";
+//output: index
 
- 
+
 
 //  function deweyBookSearch(library, num, title, start, end) {
 //     let start = start === undefined ? 0 : start;
@@ -58,95 +60,95 @@
 //     }
 //  }
 
- /**
-  * # 4
-  * 
-  * 
+/**
+ * # 4
+ * 
+ * 
 1) Given a binary search tree whose in-order and pre-order 
 traversals are respectively 14 15 19 25 27 35 79 89 90 91 and 
 35 25 15 14 19 27 89 79 91 90. What would be its postorder 
 traversal?
- answer: 14, 19, 15, 27, 25, 79, 90, 91, 89, 35
+answer: 14, 19, 15, 27, 25, 79, 90, 91, 89, 35
 
 2) The post order traversal of a binary search tree 
 is 5 7 6 9 11 10 8. What is its pre-order traversal?
- answer: 8, 6, 5, 7, 10, 9, 11
-  * 
-  */
+answer: 8, 6, 5, 7, 10, 9, 11
+ * 
+ */
 
- class BinarySearchTree {
-    constructor(key=null, value=null, parent=null) {
-      this.key = key;
-      this.value = value;
-      this.parent = parent;
-      this.left = null;
-      this.right = null;
-    }
-  
-    insert(key, value) {
-      if (this.key === null) {
-        this.key = key;
-        this.value = value;
-      }
-      else if (key < this.key) {
-          
-        if (this.left === null) {
-          this.left = new BinarySearchTree(key, value, this);
-        }
-          
-        else {
-          this.left.insert(key, value);
-        }
-      }
-      
-      else {
-        if (this.right === null) {
-          this.right = new BinarySearchTree(key, value, this);
-        }
-        else {
-          this.right.insert(key, value);
-        }
-      }
-    }
-  
-    _replaceWith(node) {
-      if (this.parent) {
-        if (this === this.parent.left) {
-          this.parent.left = node;
-        }
-        else if (this === this.parent.right) {
-          this.parent.right = node;
-        }
-  
-        if (node) {
-          node.parent = this.parent;
-        }
-      }
-      else {
-        if (node) {
-          this.key = node.key;
-          this.value = node.value;
-          this.left = node.left;
-          this.right = node.right;
-        }
-        else {
-          this.key = null;
-          this.value = null;
-          this.left = null;
-          this.right = null;
-        }
-      }
-    }
-    _findMin() {
-      if (!this.left) {
-        return this;
-      }
-      return this.left._findMin();
-    }
-  
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
   }
 
-  function binaryTreeTest() {
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (key < this.key) {
+
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+
+      else {
+        this.left.insert(key, value);
+      }
+    }
+
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+
+}
+
+function binaryTreeTest() {
 
   let Tree = new BinarySearchTree();
 
@@ -162,22 +164,103 @@ is 5 7 6 9 11 10 8. What is its pre-order traversal?
   Tree.insert(90, null)
 
   return postOrder(Tree);
-  }
+}
 
-  function postOrder(tree) {
-      if (tree.left) {
-          postOrder(tree.left)
-      }
-      if (tree.right) {
-          postOrder(tree.right)
-      }
-      console.log(tree.key);
-  }
+// function postOrder(tree) {
+//   if (tree.left) {
+//     postOrder(tree.left)
+//   }
+//   if (tree.right) {
+//     postOrder(tree.right)
+//   }
+//   console.log(tree.key);
+// }
 
 //  console.log(binaryTreeTest());
 
+// 5. Implement tree traversals
 
-  
+const tree5 = new BinarySearchTree();
+tree5.insert(25, null);
+tree5.insert(15, null);
+tree5.insert(50, null);
+tree5.insert(10, null);
+tree5.insert(24, null);
+tree5.insert(35, null);
+tree5.insert(70, null);
+tree5.insert(4, null);
+tree5.insert(12, null);
+tree5.insert(18, null);
+tree5.insert(31, null);
+tree5.insert(44, null);
+tree5.insert(66, null);
+tree5.insert(90, null);
+tree5.insert(22, null);
 
+function inOrder(tree) {
+  if (tree.left) {
+    inOrder(tree.left)
+  }
+  console.log(tree.key);
+  if (tree.right) {
+    inOrder(tree.right)
+  }
+}
 
+function preOrder(tree) {
+  console.log(tree.key);
+  if (tree.left) {
+    preOrder(tree.left)
+  }
+  if (tree.right) {
+    preOrder(tree.right)
+  }
+}
 
+function postOrder(tree) {
+  if (tree.left) {
+    postOrder(tree.left)
+  }
+  if (tree.right) {
+    postOrder(tree.right)
+  }
+  console.log(tree.key);
+}
+
+// postOrder(tree5);
+
+// 6. Find the next commanding officer
+
+const command = new BinarySearchTree();
+command.insert(5, 'Cpt Picard');
+command.insert(6, 'Cmdr Data');
+command.insert(3, 'Cmdr Riker');
+command.insert(2, 'Lt Cmdr Worf');
+command.insert(4, 'Lt Cmdr LaForge');
+command.insert(8, 'Lt Cmdr Crusher');
+command.insert(1, 'Lt Security-Officer');
+command.insert(7, 'Lt Selar');
+
+function listOfficers(tree, result = []) {
+  const queue = new Queue();
+  const node = tree;
+  console.log(node);
+  queue.enqueue(node);
+  console.log(queue);
+
+  while(queue.first) {
+    const node = queue.dequeue();
+    result.push(node.value);
+
+    if (node.left) {
+      queue.enqueue(node.left);
+    }
+    if (node.right) {
+      queue.enqueue(node.right);
+    }
+  }
+
+  return result;
+}
+
+console.log(listOfficers(command));
